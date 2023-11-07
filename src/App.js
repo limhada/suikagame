84a6e0bc9f5b6b637f4ffe210b4a3be54e196b2f
limhada <leem1315@gmail.com>
2023-11-07 21:38:02 +0900

    컴포넌트화 시키기

--- src/App.js
+++ src/App.js
@@ -1,89 +1,13 @@
-// TODO: 화면 가운데 정렬시키기
-// TODO: 추후 이미지 바꾸기
-
-import React, { useEffect, useRef } from 'react';
-import Matter from 'matter-js';
-import { FRUITS_BASE } from './fruits';
+import React from 'react';
+import Main from './main';
 
 const App = () => {
-  const matterCanvasRef = useRef(null);
-
-  useEffect(() => {
-    const { Bodies, Engine, Render, Runner, World } = Matter;
-
-    const engine = Engine.create();
-    const render = Render.create({
-      canvas: matterCanvasRef.current,
-      engine: engine,
-      options: {
-        wireframes: false,
-        background: '#F7F4C8',
-        width: 620,
-        height: 850,
-      },
-    });
-
-    const world = engine.world;
-
-    // 왼쪽으로부터 15만큼 떨어지고, 왼쪽 벽의 중앙 부분이 395 ( * 참고 matter-js는 중앙부분으로 계산함 )
-    // 왼쪽 벽
-    const leftWall = Bodies.rectangle(15, 395, 30, 790, {
-      isStatic: true,
-      render: { fillStyle: '#E6B143' },
-    });
-    // 오른쪽 벽
-    const rightWall = Bodies.rectangle(605, 395, 30, 790, {
-      isStatic: true,
-      render: { fillStyle: '#E6B143' },
-    });
-    // 바닥
-    const ground = Bodies.rectangle(310, 820, 620, 60, {
-      isStatic: true,
-      render: { fillStyle: '#E6B143' },
-    });
-
-    const topLine = Bodies.rectangle(310, 150, 620, 2, {
-      isStatic: true,
-      isSensor: true,
-      render: { fillStyle: '#E6B143' },
-    });
-
-    World.add(world, [leftWall, rightWall, ground, topLine]);
-
-    Render.run(render);
-    Runner.run(engine);
-
-    let currentBody = null;
-    let currentFruit = null;
-
-    function addFruit() {
-      const index = Math.floor(Math.random() * 5);
-      const fruit = FRUITS_BASE[index];
-
-      const body = Bodies.circle(300, 50, fruit.radius, {
-        index: index,
-        isSleeping: true, // 과일이 떨어지지 않고 준비된 상태를 만들기 위함
-        render: {
-          sprite: { texture: `${fruit.name}.png` },
-        },
-        restitution: 0.3,
-      });
-
-      currentBody = body;
-      currentFruit = fruit;
-
-      World.add(world, body);
-    }
-
-    addFruit();
-
-    return () => {
-      Render.stop(render);
-      Runner.stop(engine);
-    };
-  }, []);
-
-  return <canvas ref={matterCanvasRef} />;
+  return (
+    <div className='App'>
+      
+      <Main></Main>
+    </div>
+  )
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,115 +1,89 @@
-import React, { useState, useEffect } from "react";
-import { useMatter } from "react-matter-js";
-import { Body, Events } from "matter-js";
+// TODO: 화면 가운데 정렬시키기
+// TODO: 추후 이미지 바꾸기
+
+import React, { useEffect, useRef } from 'react';
+import Matter from 'matter-js';
+import { FRUITS_BASE } from './fruits';
 
 const Main = () => {
-  const [THEME, setTHEME] = useState("halloween");
-  const [FRUITS, setFRUITS] = useState([]);
-
-  const { world, engine, render } = useMatter();
+  const matterCanvasRef = useRef(null);
 
   useEffect(() => {
-    switch (THEME) {
-      case "halloween":
-        setFRUITS(["pumpkin", "ghost", "witch"]);
-        break;
-      default:
-        setFRUITS(["apple", "banana", "orange"]);
-    }
-  }, [THEME]);
+    const { Bodies, Engine, Render, Runner, World } = Matter;
 
-  const addFruit = () => {
-    const index = Math.floor(Math.random() * FRUITS.length);
-    const fruit = FRUITS[index];
-
-    const body = world.addBody({
-      type: "circle",
-      position: { x: 300, y: 50 },
-      radius: fruit.radius,
-      index: index,
-      isSleeping: true,
-      sprite: { texture: `${fruit.name}.png` },
+    const engine = Engine.create();
+    const render = Render.create({
+      canvas: matterCanvasRef.current,
+      engine: engine,
+      options: {
+        wireframes: false,
+        background: '#F7F4C8',
+        width: 620,
+        height: 850,
+      },
     });
 
-    return body;
-  };
+    const world = engine.world;
 
-  const handleKeyDown = (event) => {
-    const currentBody = world.bodies.find(body => body.index === FRUITS.length - 1);
-
-    switch (event.code) {
-      case "KeyA":
-        if (currentBody.position.x - currentBody.radius > 30) {
-          Body.setPosition(currentBody, { x: currentBody.position.x - 1 });
-        }
-        break;
-      case "KeyD":
-        if (currentBody.position.x + currentBody.radius < 590) {
-          Body.setPosition(currentBody, { x: currentBody.position.x + 1 });
-        }
-        break;
-      case "KeyS":
-        currentBody.isSleeping = false;
-
-        setTimeout(() => {
-          addFruit();
-        }, 1000);
-        break;
-    }
-  };
-
-  const handleCollisionStart = (event) => {
-    const currentBody = world.bodies.find(body => body.index === FRUITS.length - 1);
-
-    event.pairs.forEach(collision => {
-      if (collision.bodyA.index === collision.bodyB.index) {
-        const index = collision.bodyA.index;
-
-        if (index === FRUITS.length - 1) {
-          return;
-        }
-
-        world.removeBody(collision.bodyA);
-        world.removeBody(collision.bodyB);
-
-        const newFruit = FRUITS[index + 1];
-
-        const newBody = world.addBody({
-          type: "circle",
-          position: collision.supports[0],
-          radius: newFruit.radius,
-          index: index + 1,
-          sprite: { texture: `${newFruit.name}.png` },
-        });
-
-        if (newBody.position.y < 150) {
-          alert("Game over!");
-        }
-      }
+    // 왼쪽으로부터 15만큼 떨어지고, 왼쪽 벽의 중앙 부분이 395 ( * 참고 matter-js는 중앙부분으로 계산함 )
+    // 왼쪽 벽
+    const leftWall = Bodies.rectangle(15, 395, 30, 790, {
+      isStatic: true,
+      render: { fillStyle: '#E6B143' },
     });
-  };
+    // 오른쪽 벽
+    const rightWall = Bodies.rectangle(605, 395, 30, 790, {
+      isStatic: true,
+      render: { fillStyle: '#E6B143' },
+    });
+    // 바닥
+    const ground = Bodies.rectangle(310, 820, 620, 60, {
+      isStatic: true,
+      render: { fillStyle: '#E6B143' },
+    });
 
-  useEffect(() => {
-    render.run();
-    engine.run();
+    const topLine = Bodies.rectangle(310, 150, 620, 2, {
+      isStatic: true,
+      isSensor: true,
+      render: { fillStyle: '#E6B143' },
+    });
 
-    window.addEventListener("keydown", handleKeyDown);
-    Events.on(engine, "collisionStart", handleCollisionStart);
+    World.add(world, [leftWall, rightWall, ground, topLine]);
+
+    Render.run(render);
+    Runner.run(engine);
+
+    let currentBody = null;
+    let currentFruit = null;
+
+    function addFruit() {
+      const index = Math.floor(Math.random() * 5);
+      const fruit = FRUITS_BASE[index];
+
+      const body = Bodies.circle(300, 50, fruit.radius, {
+        index: index,
+        isSleeping: true, // 과일이 떨어지지 않고 준비된 상태를 만들기 위함
+        render: {
+          sprite: { texture: `${fruit.name}.png` },
+        },
+        restitution: 0.3,
+      });
+
+      currentBody = body;
+      currentFruit = fruit;
+
+      World.add(world, body);
+    }
+
+    addFruit();
 
     return () => {
-      render.stop();
-      engine.stop();
-
-      window.removeEventListener("keydown", handleKeyDown);
-      Events.off(engine, "collisionStart", handleCollisionStart);
+      Render.stop(render);
+      Runner.stop(engine);
     };
   }, []);
 
-  return (
-    <div>
-      <canvas ref={render.element} />
-    </div>
-  );
+  return <canvas ref={matterCanvasRef} />;
 };
 
 export default Main;
